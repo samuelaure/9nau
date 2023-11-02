dec941a8f5b92318a259cd647bb6dca9affd621a
Samŭ <samuelaure@gmail.com>
2023-11-02 23:36:05 +0100

    9ng - Develop Transactions Component

--- src/app/transactions/transactions.component.ts
+++ src/app/transactions/transactions.component.ts
@@ -1,8 +1,145 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as bootstrap from 'bootstrap';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { DatabaseService, Transaction } from '../../database/database.service';
 
 @Component({
   selector: 'app-transactions',
   templateUrl: './transactions.component.html',
   styleUrls: ['./transactions.component.scss'],
 })
-export class TransactionsComponent {}
+export class TransactionsComponent implements OnInit {
+  newTransactionForm: FormGroup;
+  transactions: Transaction[] = [];
+  contacts: any[] = [];
+  showDecimals = false;
+  decimalLimit = 2;
+  transactionsSelected: any[] = [];
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private databaseService: DatabaseService
+  ) {
+    this.newTransactionForm = this.formBuilder.group({
+      date: [new Date().toISOString().split('T')[0], Validators.required],
+      from: ['BAHUG', Validators.required],
+      to: ['WOSE', Validators.required],
+      concept: ['TEST CONCEPT', Validators.required],
+      amount: ['333', Validators.required],
+    });
+  }
+
+  ngOnInit(): void {
+    this.onInit();
+  }
+
+  onInit() {
+    this.databaseService.getData().subscribe((data: any) => {
+      this.transactions = data.transactions.sort(
+        (
+          a: { date: string | number | Date },
+          b: { date: string | number | Date }
+        ) => {
+          const dateA = new Date(a.date).getTime();
+          const dateB = new Date(b.date).getTime();
+          return dateA - dateB;
+        }
+      );
+      this.contacts = data.contacts;
+    });
+  }
+
+  formatDate(date: string): string {
+    return new Date(date).toLocaleDateString();
+  }
+
+  calculateBalance(index: number): number {
+    const transactionsSlice = this.transactions.slice(0, index + 1);
+    return transactionsSlice.reduce((acc, t) => acc + t.amount, 0);
+  }
+
+  getAmountClass(value: number): string {
+    const roundedValue = value >= 0 ? Math.floor(value) : Math.ceil(value);
+
+    if (roundedValue > 0) {
+      return 'text-success';
+    } else if (roundedValue < 0) {
+      return 'text-danger';
+    } else {
+      return 'text-secondary';
+    }
+  }
+
+  toggleDecimals() {
+    this.showDecimals = !this.showDecimals;
+  }
+
+  getRoundedAmount(value: number): string | number {
+    if (!this.showDecimals) {
+      const roundedValue = value >= 0 ? Math.floor(value) : Math.ceil(value);
+      return Number.isInteger(roundedValue)
+        ? roundedValue
+        : value.toFixed(this.decimalLimit);
+    }
+    return value.toFixed(this.decimalLimit);
+  }
+
+  openTransactionForm() {
+    const modalElement = document.getElementById('transactionModal');
+    if (modalElement) {
+      const transactionModal = new bootstrap.Modal(modalElement);
+      transactionModal.show();
+    } else {
+      console.error('Modal element not found.');
+    }
+  }
+
+  createNewTransaction() {
+    if (this.newTransactionForm.valid) {
+      const newTransaction = {
+        date: new Date(this.newTransactionForm.value.date).toISOString(),
+        from: this.newTransactionForm.value.from,
+        to: this.newTransactionForm.value.to,
+        concept: this.newTransactionForm.value.concept,
+        amount: Number(this.newTransactionForm.value.amount),
+      };
+      console.log(new Date(this.newTransactionForm.value.date).toISOString());
+
+      console.log(newTransaction);
+
+      this.databaseService.createTransaction(newTransaction).subscribe(() => {
+        console.log('Database updated successfully.');
+        this.onInit();
+      });
+
+      this.newTransactionForm.reset({
+        date: new Date().toISOString().split('T')[0],
+        concept: 'TEST CONCEPT',
+      });
+    }
+  }
+
+  deleteTransaction(index: number) {
+    this.databaseService.deleteTransaction(index).subscribe(() => {
+      console.log('Database updated successfully.');
+      this.onInit();
+    });
+  }
+
+  checkboxChanged(event: any, index: number) {
+    if (event.target.checked) {
+      this.transactionsSelected.push(index);
+    } else {
+      this.transactionsSelected = this.transactionsSelected.filter(
+        i => i !== index
+      );
+    }
+    console.log(this.transactionsSelected);
+  }
+
+  bulkDeleteTransactions() {
+    for (const index of this.transactionsSelected) {
+      this.deleteTransaction(index);
+    }
+  }
+}
