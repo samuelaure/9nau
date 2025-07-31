9f7449626abe8611ea9aaf3dee745d39625a4f08
Samuel Aure <samuelaure@gmail.com>
2025-07-31 11:44:05 +0200

    chore(repo): update development configurations

--- eslint.config.js
+++ eslint.config.js
@@ -3,6 +3,17 @@ const tseslint = require('typescript-eslint')
 const prettierConfig = require('eslint-config-prettier')
 
 module.exports = tseslint.config(
+  {
+    ignores: [
+      '**/node_modules/', 
+      '**/dist/',         
+      '**/out/',          
+      '**/.next/',        
+      '**/coverage/',     
+      '**/generated/',    
+    ],
+  },
+  
   eslintJs.configs.recommended,
   ...tseslint.configs.recommended,
   prettierConfig
