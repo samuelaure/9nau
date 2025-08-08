100684bb4b6b8e6a36a2d3a016f8d7a1e5beaeda
Samuel Aure <samuelaure@gmail.com>
2025-08-08 12:37:50 +0200

    fix: Solve some of many test running errors and refactor code structure for improved readability and maintainability

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,8 @@
 module.exports = {
   projects: [
-    '<rootDir>/apps/api/jest.config.js',
-    '<rootDir>/apps/web/jest.config.js',
-    '<rootDir>/packages/core/jest.config.js',
-    '<rootDir>/packages/ui/jest.config.js',
+    '<rootDir>/apps/api',
+    '<rootDir>/apps/web',
+    '<rootDir>/packages/core',
+    '<rootDir>/packages/ui',
   ],
 };
