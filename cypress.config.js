e4078245c6b9b94ac42a8c71aae1c11ad4a7c70c
Samuel Aure <samuelaure@gmail.com>
2025-08-16 05:54:32 +0200

    feat: Centralize environment variables

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,13 @@
 const { defineConfig } = require("cypress");
+const dotenv = require('dotenv');
+const path = require('path');
+
+dotenv.config({ path: path.resolve(__dirname, './.env.test') });
 
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      // node event listeners here
     },
     specPattern: "apps/web/cypress/e2e/**/*.cy.{js,jsx,ts,tsx}",
     supportFile: "apps/web/cypress/support/e2e.ts",
